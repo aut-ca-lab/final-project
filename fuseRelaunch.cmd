f0b88dab61cae951967b2dd457f57f88516d08d0
Radin Shayanfar <radin.shayanfar@gmail.com>
2020-09-27 19:45:14 +0330

    files moved to root directory

---
+++ fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "secureip" -o "E:/Computer Architecture/ca_fp/uut_main_board_isim_beh.exe" -prj "E:/Computer Architecture/ca_fp/uut_main_board_beh.prj" "work.uut_main_board" 
